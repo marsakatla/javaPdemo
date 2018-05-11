f21235fcddc51faa9dfc5faf63179bcec4b5fdb1
tsuser <tsuser@IT-ADMIN-PC>
2018-05-11 15:52:34 +0530

    commited by rk

--- Insurance/src/com/ts/InsuranceDAO.java
+++ Insurance/src/com/ts/InsuranceDAO.java
@@ -7,25 +7,39 @@ import java.sql.Statement;
 
 public class InsuranceDAO
 {
-	public String isUser(String uid,String pwd)
+	public UserBean isUser(String uid,String pwd)
+	{
+		UserBean details = new UserBean();
+	
 	{String role="";
 		try
 		{
-			Class.forName("com.mysql.jdbc.driver");
-			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3036/rk","root","root");
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/rk","root","root");
 			
 			Statement stmt = con.createStatement();
 			ResultSet rs=stmt.executeQuery("select role from underwriter where user_id='"+uid+"' and password='"+pwd+"'");
 			while(rs.next())
-				{role=rs.getString(1);}
+			{
+				details.setRole(rs.getString(1));
+				details.setUsername(rs.getString(2));
+			}
+				//{role=rs.getString(1);}
 			rs.close();
 			con.close();
-			return role;
+			//return role;
 		}
 		catch(Exception e)
 		{
-			e.printStackTrace();
-			return role;
+			System.out.println(e);
+		}
+		finally 
+		{
+			return details;
+		}
+			//e.printStackTrace();
+			//return role;
+	}
 		}
 		
 	}
